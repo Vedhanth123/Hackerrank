98342dcfcb9d31f1c5f556a7d8716c16b88993b1
Vedhanth <gvedhanth123@outlook.com>
2021-06-25 20:06:21 +0530

    Commit from VS code

---
+++ Jumping_on_clouds.py
@@ -0,0 +1,25 @@
+
+def jumpingOnClouds(c, k):
+
+
+    # energy level
+    e = 100
+
+    # variable for index of array
+    i = 0
+
+    while(i == 0 and e < 100):
+
+        # increment the index of the array by two
+        i = (i+k)%len(c)
+
+        # cloud landed on is a thunder cloud then decrement energy by 3
+        if(c[i] == 1):
+            e -= 3
+        else:
+            e -= 1
+
+    return e
+
+jumpingOnClouds([0, 0, 1, 0, 0, 1, 1, 0], 2) 
+    
