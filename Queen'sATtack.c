54555ee48aea553d4f3aef270e37de39b0c9f1be
Vedhanth <gvedhanth123@outlook.com>
2021-06-29 23:08:21 +0530

    Second commit of Queens attack

--- Queen'sATtack.c
+++ Queen'sATtack.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 void traversing(int r_q, int c_q, int **chessboard, int n)
@@ -24,17 +25,23 @@ void traversing(int r_q, int c_q, int **chessboard, int n)
         
 
     // 2) traversing queen downward
-    row = r_q+1
-    col = c_q
+    row = r_q+1;
+    col = c_q;
 
 
-    while((row < n && row >= 0)):
+    while((row < n && row >= 0))
+    {
+        if(chessboard[row][col] != -1)
+        {
+            chessboard[row][col] = 1;
+            row += 1;
+        }
+        else
+        {
+            break;
+        }
+    }
 
-        if(chessboard[row][col] != -1):
-            chessboard[row][col] = 1
-            row += 1
-        else:
-            break
     
     // 3) traversing queen righside
     row = r_q;
@@ -80,97 +87,127 @@ void traversing(int r_q, int c_q, int **chessboard, int n)
     row = r_q-1;
     col = c_q+1;
     
-    while((col < n && col >= 0) && (row < n && row >= 0)):
-        
-        if(chessboard[row][col] != -1):
+    while((col < n && col >= 0) && (row < n && row >= 0))
+    {
+        if(chessboard[row][col] != -1)
+        {
             chessboard[row][col] = 1;
             col += 1;
             row -= 1;
-        else:
+        }
+        else
+        {
             break;
+        }
+    }
+        
         
     // 6) traversing queen downward right
     row = r_q+1;
     col = c_q+1;
     
-    while((col < n && col >= 0) && (row < n && row >= 0)):
+    while((col < n && col >= 0) && (row < n && row >= 0))
+    {
         
-        if(chessboard[row][col] != -1):
+        if(chessboard[row][col] != -1)
+        {
             chessboard[row][col] = 1;
             col += 1;
             row += 1;
-        else:
-            break
+        }
+        else
+        {
+            break;
+        }
+    }
 
     // 7) traversing queen upward left
     row = r_q-1;
     col = c_q-1;
     
-    while((col < n && col >= 0) && (row < n && row >= 0)):
-        
-        if(chessboard[row][col] != -1):
+    while((col < n && col >= 0) && (row < n && row >= 0))
+    {
+        if(chessboard[row][col] != -1)
+        {
             chessboard[row][col] = 1;
             col -= 1;
             row -= 1;
-        else:
+        }
+        else
+        {
             break;
+        }
+    }
+        
         
     // 8) traversing queen downward left
     row = r_q+1;
     col = c_q-1;
     
-    while((col < n && col >= 0) && (row < n && row >= 0)):
+    while((col < n && col >= 0) && (row < n && row >= 0))
+    {
+
         
-        if(chessboard[row][col] != -1):
+        if(chessboard[row][col] != -1)
+        {
             chessboard[row][col] = 1;
             col -= 1;
             row += 1;
-        else:
+        }
+        else
+        {
             break;
+        }
+    }
 }
 
-def counting(chessboard, n):
+int counting(int **chessboard, int n)
+{
+    int count = 0;
 
-    count = 0;
-
-    for x in range(n):
-
-        for y in range(n):
-
-            if(chessboard[x][y] == 1):
+    for(int x= 0; x<n; x++)
+    {
+        for(int y= 0; y < n; y ++)
+        {
+            if(chessboard[x][y] == 1)
+            {
                 count += 1;
-    
-    return count;
+            }
+        }
+    }
 
-def queensAttack(n, k, r_q, c_q, obstacles):
+    return count;
+}
+
+
+int queensAttack(int n, int k, int r_q, int c_q, int obstacles_rows, int obstacles_columns, int** obstacles)
+{
+
 
     // 1) create a chessboard with linked_list
+    int *chessboard[n];
+    for(int i = 0;i < n; i++)
+    {
+        chessboard[i] = (int*) calloc(n, sizeof(int)); 
+    }
 
-    chessboard = []
-
-    // 1.1) inserting zeros into the chessboard
-    for y in range(n):
-
-        // 1.2) create an intermediary array
-        chessboard.append([])
-        for x in range(n):
-
-            chessboard[y].append(0)
-    
 
     // 2) change the co ordinates of the obstacles to the chessboard we've created
-    for x in range(len(obstacles)):
-
+    for(int i = 0; i< k; i ++)
+    {
         // 2.1) subtract row of obstacle from n to change the co ordinates of the obstacle
-        obstacles[x][0] = n - obstacles[x][0]
+        obstacles[i][0] = n - obstacles[i][0];
 
         // 2.2) subtract 1 from the row to change the co ordinates of the obstacle
-        obstacles[x][1] = obstacles[x][1] - 1
+        obstacles[i][1] = obstacles[i][1] - 1;
+    }
 
     // 3) place obstacles on the chessboard
-    for y in range(len(obstacles)):
+    for(int y = 0; y < k; y ++)
+    {
+        chessboard[obstacles[y][0]][obstacles[y][1]] = -1;
+    }
 
-        chessboard[obstacles[y][0]][obstacles[y][1]] = -1
 
     // 4) change the co ordinates of the queen to the chessboard
     
@@ -187,12 +224,12 @@ def queensAttack(n, k, r_q, c_q, obstacles):
     traversing(r_q, c_q, chessboard, n);
 
     // 7) find the no of blocks queen has traversed
-    blocks_traversed = counting(chessboard, n);
+    return counting(chessboard, n);
+}
 
-    return blocks_traversed;
-
-
-queensAttack(5,3,4,3, [[5, 5], [4, 2], [2, 3]])
-
+int main()
+{
+    
+}
 
 
