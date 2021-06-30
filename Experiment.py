3691ee603ddbc3f5d42f9edc405cf5294e3f2b95
Vedhanth <gvedhanth123@outlook.com>
2021-06-30 09:48:17 +0530

    Second day commit

--- Experiment.py
+++ Experiment.py
@@ -1,23 +1,3 @@
-# chessboard = {1 : {1:0, 2:0, 3:0, 4:0, 5:0}, 2 : {1:0, 2:0, 3:0, 4:0, 5:0}}
+# Don't construct a chessboard with arrays
 
-
-# for x in range(1,3):
-
-#     for y in range(1,6):
-
-#         print(chessboard[x][y], end= "")
-
-#     print("")
-n = 5
-
-obstacles = [[5, 5], [4, 2], [2, 3]]
-# create a empty dictionary and allocate memory according to the size of the board given
-
-chessboard = {}
-
-chessboard[1] = {1:0,2:0}
-
-chessboard[1][1] = 1
-
-print(chessboard)
-print(chessboard[1][1])
+# traver
