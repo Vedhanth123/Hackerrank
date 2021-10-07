eb04170320713968f2b3dff9e427d9d40e0e6432
Vedhanth123 <gvedhanth123@outlook.com>
2021-10-07 23:12:56 +0530

    Luck Balance

--- Luck_balance.py
+++ Luck_balance.py
@@ -1,4 +1,6 @@
 
+"""
+
 # 1) given how many matches can be lost of important
 
 n = 6
@@ -24,3 +26,40 @@ for i in range(len(contests)):
 
     if(contests[i][0] != 1):
         
+"""
+
+def luckBalance(k, contests):
+    # Write your code here
+    
+    # Lena should lose important matches which have high luck rating
+    # Lena will obviously lose all the umimportant and addes up luck rating to the total luck.
+
+    TotalLuckRatingOfLena = 0
+
+    # 1) first add up the luck ratings of all the unimportant matches
+    for x in range(len(contests)):
+
+        if(contests[x][1] == 0):
+            TotalLuckRatingOfLena += contests[x][0]
+        
+    HighestLuckRating = contests[x][1]
+
+    for z in range(k):
+        for y in range(len(contests)):
+
+            # 2) find out the important match which has the highest luck rating
+            if(contests[x][1] == 1):
+                if(HighestLuckRating < contests[x][1]):
+                    HighestLuckRating = contests[x][1]
+            
+        for x in range(len(contests)):
+
+            if(contests[x][1] == HighestLuckRating):
+                TotalLuckRatingOfLena = HighestLuckRating
+                contests[x][1] = 0
+                
+luckBalance(3, [[5, 1], [2, 1], [1, 1], [8, 1], [10, 0], [5, 0]])
+
+    
+
+    pass
--- Mergesort.py
+++ Mergesort.py
@@ -1,64 +1,67 @@
+class MergeSort:
 
+    # 2) Merge the divided array into one single array
+    def Merge(self, array, l, mid, r):
 
-# 2) Merge the divided array into one single array
-def Merge(array, l, mid, r):
+        # loop invariant for left array
+        i = l
 
-    # loop invariant for left array
-    i = l
+        # loop invariant for right arrray
+        j = mid + 1
 
-    # loop invariant for right arrray
-    j = mid + 1
+        # loop invariant for original array
+        k = l
+        
+        # backing up array into left and right
+        left = array[l: mid + 1]
+        right = array[mid + 1:r + 1]
+        
+        # sorting out the array
+        while(i <= mid and j <= r):
 
-    # loop invariant for original array
-    k = l
-    
-    # backing up array into left and right
-    left = array[l: mid + 1]
-    right = array[mid + 1:r + 1]
-    
-    # sorting out the array
-    while(i <= mid and j <= r):
-
-        if(left[i - l] < right[j - (mid + 1)]):
+            if(left[i - l] < right[j - (mid + 1)]):
+                array[k] = left[i - l]
+                k += 1
+                i += 1
+            else:
+                array[k] = right[j - (mid + 1)]
+                j += 1
+                k += 1
+        
+        # inserting the left over elements into their correct positions
+        while(i <= mid):
             array[k] = left[i - l]
-            k += 1
             i += 1
-        else:
+            k += 1
+        while(j <= r):
             array[k] = right[j - (mid + 1)]
             j += 1
             k += 1
-    
-    # inserting the left over elements into their correct positions
-    while(i <= mid):
-        array[k] = left[i - l]
-        i += 1
-        k += 1
-    while(j <= r):
-        array[k] = right[j - (mid + 1)]
-        j += 1
-        k += 1
 
-# 1) divide the array into two parts left and right
-def Mergesort(array, l, r):
+    # 1) divide the array into two parts left and right
+    def Mergesort(self, array, l, r):
 
-    # if the left part surpasses or equals to the right part then stop 
-    # recursion
-    if(l >= r):
-        return array
-    
-    else:
-
+        # if the left part surpasses or equals to the right part then stop 
+        # recursion
+        if(l >= r):
+            return array
         
-        mid = ((l + r)// 2)
+        else:
 
-        # recursive case for the left and right part of the array
-        Mergesort(array, l, mid)
-        Mergesort(array, mid + 1, r)
+            
+            mid = ((l + r)// 2)
 
-        # Merging the array
-        Merge(array, l, mid, r)
+            # recursive case for the left and right part of the array
+            self.Mergesort(array, l, mid)
+            self.Mergesort(array, mid + 1, r)
+
+            # Merging the array
+            self.Merge(array, l, mid, r)
+
 
 array = [6,3,9,5,2,8,7,1,0]
-Mergesort(array, 0, 8)
+obj = MergeSort()
+
+obj.Mergesort(array, 0, 8)
 
 print(array)
